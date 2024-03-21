b7e880d2ccf1a306199ed27cc6a0c8b1cf78f5f4
RohitRaj766 <rohitraj2k04@gmail.com>
2024-03-21 12:25:30 +0530

    resgistration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -12,10 +12,10 @@ export default function Registration() {
           IMPORTANT DATES !
         </h4>
         <p data-aos="fade-down">
-          Last Date for Submission of Full Paper: 15.02.2022 <br />
-          Notification of Acceptance of Full Paper: 18.02.2022
+          Last Date for Submission of Full Paper: 2/04/2024 <br />
+          Notification of Acceptance of Full Paper : 12/4/2024
           <br />
-          Last Date for Registration: 26.02.2022{" "}
+          Last Date for Registration: 16/04/2023{" "}
         </p>
         <hr />
       </div>
@@ -35,23 +35,23 @@ export default function Registration() {
           <tbody>
             <tr>
               <td> Corporates / Industry Experts</td>
-              <td> Rs. 3000/-</td>
-              <td> USD - 40</td>
+              <td> Rs. 2000/-</td>
+              <td> USD - 28</td>
             </tr>
             <tr>
               <td> Faculty / Academicians </td>
-              <td> Rs. 2000/-</td>
-              <td> USD - 26</td>
+              <td> Rs. 1000/-</td>
+              <td> USD - 13</td>
             </tr>
             <tr>
               <td> Research Scholars</td>
-              <td> Rs. 1500/-</td>
-              <td> USD - 19</td>
+              <td> Rs. 500/-</td>
+              <td> USD - 7</td>
             </tr>
             <tr>
-              <td> Students</td>
-              <td> Rs. 1000/-</td>
-              <td> USD - 13</td>
+              <td> Students/Co-Author</td>
+              <td> Rs. 500/-</td>
+              <td> USD - 7</td>
             </tr>
           </tbody>
         </table>
@@ -68,10 +68,10 @@ export default function Registration() {
           <p>
             Online Transfer / Bank Transfer
             <br />
-            Bank Name: Bank of Maharashtra <br />
-            Branch Name: Bistupur, Jamshedpur <br />
-            Account Name: ARKA JAIN UNIVERSITY <br />
-            Account Number: 60273286728 IFSC CODE: MAHB0001241
+            Bank Name : HDFC BANK <br />
+            Branch Name : CHOTA Gamharia <br />
+            Account Name : ARKA JAIN UNIVERSITY <br />
+            Account Number : 50100459210580 IFSC Code: HDFC0004801
           </p>
 
           <hr />
@@ -84,18 +84,17 @@ export default function Registration() {
             <div class="card-header py-3 text-white bg-success border-dark">
               <h5 class="my-0 fw-normal"> Corporates/IndustryExperts</h5>
             </div>
+              <a href="https://forms.gle/e1jdEmoBfm5YXT5R9" style={{textDecoration: 'none', color: 'inherit'}}>
             <div class="card-body">
-              <h5 class="card-title pricing-card-title">RS : 3000</h5>
-              <h5 class="card-title pricing-card-title">USD : $40</h5>
+              <h5 class="card-title pricing-card-title">RS : 2000</h5>
+              <h5 class="card-title pricing-card-title">USD : $28</h5>
               <ul class="list-unstyled mt-3 mb-4"></ul>
-              <button
-                type="button"
-                class="w-100 btn btn-lg btn-success"
-                data-aos="flip-up"
-              >
+              <button type="button" class="w-100 btn btn-lg btn-success" data-aos="flip-up">
                 Register Now
-              </button>
+            </button>
+
             </div>
+              </a>
           </div>
         </div>
 
@@ -103,19 +102,20 @@ export default function Registration() {
           <div class="card mb-4 rounded-3 shadow-sm payment_box border-dark">
             <div class="card-header py-3 text-white bg-success border-dark">
               <h5 class="my-0 fw-normal"> Faculty/Academicians</h5>
-            </div>
+            </div><a href="https://forms.gle/e1jdEmoBfm5YXT5R9" style={{textDecoration: 'none', color: 'inherit'}}>
             <div class="card-body">
-              <h5 class="card-title pricing-card-title">RS : 2000</h5>
-              <h5 class="card-title pricing-card-title">USD : $26</h5>
+              <h5 class="card-title pricing-card-title">RS : 1000</h5>
+              <h5 class="card-title pricing-card-title">USD : $13</h5>
               <ul class="list-unstyled mt-3 mb-4"></ul>
               <button
                 type="button"
                 class="w-100 btn btn-lg btn-success"
                 data-aos="flip-up"
               >
-                Register Now
+             Register Now
               </button>
             </div>
+            </a>
           </div>
         </div>
 
@@ -124,9 +124,10 @@ export default function Registration() {
             <div class="card-header py-3 text-white bg-success border-dark">
               <h5 class="my-0 fw-normal">ResearchScholars</h5>
             </div>
+            <a href="https://forms.gle/e1jdEmoBfm5YXT5R9" style={{textDecoration: 'none', color: 'inherit'}}>
             <div class="card-body">
-              <h5 class="card-title pricing-card-title">RS : 1500</h5>
-              <h5 class="card-title pricing-card-title">USD : $19</h5>
+              <h5 class="card-title pricing-card-title">RS : 500</h5>
+              <h5 class="card-title pricing-card-title">USD : $7</h5>
               <ul class="list-unstyled mt-3 mb-4"></ul>
               <button
                 type="button"
@@ -136,16 +137,18 @@ export default function Registration() {
                 Register Now
               </button>
             </div>
+            </a>
           </div>
         </div>
 
         <div class="col">
           <div class="card mb-4 rounded-3 shadow-sm payment_box border-dark">
             <div class="card-header py-3 text-white bg-success border-dark">
-              <h5 class="my-0 fw-normal">Students</h5>
+              <h5 class="my-0 fw-normal">Students/Co-Authurs</h5>
             </div>
+            <a href="https://forms.gle/e1jdEmoBfm5YXT5R9" style={{textDecoration: 'none', color: 'inherit'}}>
             <div class="card-body text-center">
-              <h5 class="card-title pricing-card-title">RS : 1000</h5>
+              <h5 class="card-title pricing-card-title">RS : 500</h5>
               <h5 class="card-title pricing-card-title">USD : $13</h5>
               <ul class="list-unstyled mt-3 mb-4">
                 <li></li>
@@ -155,9 +158,10 @@ export default function Registration() {
                 class="w-100 btn btn-lg btn-success"
                 data-aos="flip-up"
               >
-                Register Now
+               Register Now
               </button>
             </div>
+            </a>
           </div>
         </div>
       </div>
@@ -165,12 +169,10 @@ export default function Registration() {
       <div class="alert alert-success" role="alert">
         <h4 class="alert-heading dark">PUBLICATION FEE</h4>
         <p>
-          The selected paper of the International Conference will be sent to the
-          journal publishers for Paper publications. The publication charges
-          will be given by the authors as per the actual of the fees charged by
-          the publishers. The publication charges for the research papers
-          selected for the SCI/SCOPUS Indexed Journal will be communicated to
-          the respective authors.
+        The registration fee includes the cost of publications.
+        The outstanding papers in extended form (selected by Reviewer Committee) will be
+        communicated to SCI/SCOPUS Indexed Journal and the publication charges for such
+        research papers will be borne by respective authors.
         </p>
 
         <hr />
