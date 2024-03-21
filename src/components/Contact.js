a677bed74754c0b6721d058abfc8d420acb314e1
RohitRaj766 <rohitraj2k04@gmail.com>
2024-03-21 12:35:38 +0530

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,63 +3,61 @@ import Navbar from './Navbar'
 
 
 export default function Contact() {
+
+  const committeeMembers = [
+    {
+      role: 'CONFERENCE CHAIR',
+      name: 'Dr. Ashwini Kumar',
+      position: 'Asst. Dean, School of Engineering & IT',
+      email: 'ashwini.kumar@arkajainuniversity.ac.in',
+      phone: '+91-8210353795'
+    },
+    {
+      role: 'CONVENOR',
+      name: 'Dr. Binod Kumar Choudhary',
+      position: 'Associate Professor, Department of Electrical & Electronics Engineering',
+      email: 'dr.binod@arkajainuniversity.ac.in',
+      phone: '+91-9939974038'
+    },
+    {
+      role: 'CO - CONVENOR',
+      name: 'Dr ChandraPrabha Sahu',
+      position: 'Assistant Professor, Department of Electrical and Electronics Engineering',
+      email: 'dr.chandra@arkajainuniversity.ac.in',
+      phone: '+91-9128329236'
+    },
+    {
+      role: 'CONVENOR',
+      name: 'Dr Jonaki Mukherjee',
+      position: 'Assistant Professor Department of Electrical and Electronics Engineering',
+      email: 'dr.jonaki@arkajainuniversity.ac.in',
+      phone: '+91–8902464892'
+    }
+  ];
+
+
   return (
-    <div>
+    <>
 <Navbar />
-
-<div className="container py-5">
-      <div class="container-fluid row my-5">
-      <div class="col d-flex align-items-start my-3">
-        <div class="icon-square bg-danger text-dark flex-shrink-0 me-3">
-          <svg class="bi" width="1em" height="1em"></svg>
-        </div>
-        <div>
-          <h4 >CONVENOR</h4>
-          <p>Mr. Ashwini Kumar, Asst. Prof, ARKA JAIN University.<br/>
-          Email - ashwini.kumar@ arkajainuniversity.ac.in <br/>Mob. – +91-821035379</p>
-          <a href="https://mail.google.com/" class="btn btn-danger " data-aos="fade-down">
-          Email-Now
-          </a>
-        </div>
+<div className="container w-100" style={{marginTop:'100px',minHeight:'100vh'}}>
+      <div className="container-fluid row my-5">
+        {committeeMembers.map((member, index) => (
+          <div key={index} className="col d-flex align-items-start my-3">
+            <div className="icon-square bg-danger text-dark flex-shrink-0 me-3">
+              <svg className="bi" width="1em" height="1em"></svg>
+            </div>
+            <div>
+              <h4>{member.role}</h4>
+              <p>{member.name}, {member.position}<br />
+                Email - {member.email}<br />Mob. – {member.phone}</p>
+              <a href="https://mail.google.com/" className="btn btn-danger" data-aos="fade-down">
+                Email-Now
+              </a>
+            </div>
+          </div>
+        ))}
       </div>
-      
-      
-   
-
-      <div class="col d-flex align-items-start my-3">
-        <div class="icon-square bg-danger text-dark flex-shrink-0 me-3">
-          <svg class="bi" width="1em" height="1em"></svg>
-        </div>
-        <div>
-          <h4 >CO- CONVENOR</h4>
-          <p>Dr. Binod Kumar Choudhary, Asst. Prof, ARKA JAIN University.<br/>
-          Email- dr.binod@ arkajainuniversity.ac .in<br/>Mob. – +91-9939974038</p>
-          <a href="https://mail.google.com/" class="btn btn-danger" data-aos="fade-down">
-          Email-Now
-          </a>
-        </div>
-      </div>
-
-      <div class="col d-flex align-items-start my-3 ">
-<div class="icon-square bg-danger text-dark flex-shrink-0 me-3 ">
-  <svg class="bi" width="1em" height="1em"></svg>
-</div>
-<div>
-<h4 >Organising Secretary</h4>
-  <p>Dr. Anupam Kumari, Asst. Prof, ARKA JAIN University.<br/>
-  Email- dr.anupam@ arkajainuniversity.ac.in<br/>Mob. – +91-9939974038</p>
-  <a href="https://mail.google.com/" class="btn btn-danger " data-aos="fade-down">
-  Email-Now
-  </a>
-</div>
-</div>
-</div>
     </div>
-    
-    
-    
-    
-    
-    </div>
+    </>
   )
 }
