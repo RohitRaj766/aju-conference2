82e8a0be0292753a8cd67c0e58ad26450f2a16ed
RohitRaj766 <rohitraj2k04@gmail.com>
2024-03-21 12:06:02 +0530

    committee

--- src/components/Committee.js
+++ src/components/Committee.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 // import Footer from "./Footer";
-import HideShow from "./HideShow";
+// import HideShow from "./HideShow";
+import jsondata from "../utils/topics.json"
 
 export default function Committee() {
+  const advisory_committee = jsondata["advisory_committee"];
+  const reviewer_committee = jsondata["reviewer_committee"];
+  const editorial_committee = jsondata["editorial_committee"];
+  const organizing_committee = jsondata["organizing_committee"];
+  const technical_support_committee = jsondata["technical_support_committee"];
   return (
     <>
     <Navbar/>
-
         <div
         id="aboutbox_scopeofconfrence"
         className="container-fluid row featurette"
@@ -17,50 +22,9 @@ export default function Committee() {
           ADVISORY COMMITTEE<span className="text-muted"></span>
           </h2>
           <ul id="aboutajudetails" className="lead">
-          <li>Mr. Jasbir Dhanjal, Registrar, ARKA JAIN University, Jharkhand  </li>
-<li>Dr. Angad Tiwary, Dean Student’ Welfare, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Arvind Kumar, Programme Co-ordinator, ARKA JAIN University</li>
-
-<li>Dr. B Rajeev, College of Engineering, Pune</li>
-<li>Dr. P Muthukumar, IIT Guwahati</li>
-<li>Dr Abhijeet Mohapatra, IIT Kanpur</li>
-<li>Dr. Apurva Sharma, IIT Roorkee</li>
-<li>Prof. Manikant Paswan , NIT Jamshedpur</li>
-<li>Dr. Bunil Balwant Rai , NIT Meghalaya</li>
-<li>Dr. Vishesh  Ranjan  Kar, NIT Jamshedpur</li>
-<li>Dr Alekha Kumar Mishra, NIT, Jamshedpur</li>
-<li>Prof.(Dr. )Apurba Layek, NIT Durgapur, West Bengal </li>
-<li>Dr. Sanjay Kumar NIT Jamshedpur</li>
-<li>Dr. Somnath Mandel , NIT Jamshedpur</li>
-<li>Dr. Subhadeep Metya, NIT Jamshedpur</li>
-<li>Dr. Abdhesh Kumar Sinha, NIT Jamshedpur</li>
-<li>Dr. Pradip, NIT Sikkim</li>
-<li>Dr. Tripti Ranjan Mahapattra, VSSUIT , Sambalpur Orrisa</li>
-<li>Prof. (Dr.)Somnath Chhtopadyay, IIT DHANBAD</li>
-<li>Dr. Subrat Kumar Panda, NIT Rourkela</li>
-<li>Dr. Chetan Kumar Hirwani NIT, Patna</li>
-<li>Dr. Danish Ali Khan, NIT, Jamshedpur, Jharkhand</li>
-<li>Dr. Ajit Kumar, IIT (ISM), Dhanbad , Jharkhand</li>
-<li>Dr. Dilip Kumar Singh, B.I.T Mesra, Jharkhand</li>
-<li>Dr. Kavita Tapadia, NIT Raipur, Chhattisgarh</li>
-<li>Prof. (Dr.)Parmanand Bajpai,Central University Chhattisgarh, Bilaspur, Chhattisgarh</li>
-<li>Dr. P K Jena, KIIT, University, Bhubaneshwar</li>
-<li>Prof. (Dr.) Prasanna, Sharma, Amity University Raipur</li>
-<li>Dr. Gajendra Chauhan , Teerthanker Mahaveer University, Moradabad, Uttar Pradesh</li>
-<li>Dr . Alfa , NIT, Silcher, Sikkim</li>
-<li>Dr Assish Kumar Thripati, VIT University, Vellore</li>
-<li>Dr. Dilip Kumar Giri, Binod Bihari Mahto Koylanchal University, Jharkhand </li>
-<li>Dr Shilpa Sharma, BIT Raipur, Chhattisgarh </li>
-<li>Dr . Raj Kumar Singh, University Department of Physics , Ranchi University, Ranchi, Jharkhand</li>
-<li>Dr. Manoj Kumar Singh, Principal  Ram Lakhan singh Yadav College, Ranchi Jharkhand</li>
-<li>Dr. Dilip Kumar Giri, Binod Bihari Mahto Koylanchal University, Jharkhand </li>
-<li>Dr. Ram Prasad Prajapati , Jawaherlal Nehru University-JNU New Delhi, India</li>
-<li>Dr Indrajeet Kumar,S P College Dumka,SKMU University Dumka, Jharkhand, India</li>
-<li>Dr. Sudeep Chartterjee, Bennett University, Greater Noida, India</li>
-<li>Dr. Avinash Kumar, GGSTSEC, Bokaro, Jharkhand , India</li>
-<li>Dr. Sumanto Bhatacharya,  College of Engineering and Management, Greater Kolkata, India</li>
-<li>Dr. Arun Kumar, , ISBM University , Raigarh , Chhattisgarh, India</li>
-
+          {advisory_committee.map((field, index) => (
+            <li key={index}>{field}</li> // Use field itself or index as a key
+          ))}
           </ul>
         </div>
       </div>
@@ -72,15 +36,12 @@ export default function Committee() {
       >
         <div className="col-md-7 about" data-aos="fade-down">
           <h2 id="aboutajuheading" className="featurette-heading ">
-          FOREIGN MEMBERS<span className="text-muted"></span>
+          REVIEWER COMMITTEE<span className="text-muted"></span>
           </h2>
           <ul id="aboutajudetails" className="lead ">
-          <li>Prof. Dr  Mortka Zurek Marta , Institute of For Sustainable Technologies </li>
-          <li>Prof. Josua Petrus Meyer, University of Preoria, South Africa.</li>
-          <li>Dr. Rahul Klyankar, Laram Technologies , Florida</li>
-          <li>Dr. Celia Shahnaz, Department of EEE, BUET, Bangladesh</li>
-          <li>Dr Amrit Mukherjee, University of South Bohemia, Cske Budejovice, Czech Republic</li>
-          <li>Dr Nivedita Kulkurni, UCL London</li>
+          {reviewer_committee.map((field, index) => (
+            <li key={index}>{field}</li> // Use field itself or index as a key
+          ))}
           </ul>
         </div>
       </div>
@@ -94,24 +55,9 @@ export default function Committee() {
           EDITORIAL COMMITTEE<span className="text-muted"></span>
           </h2>
           <ul id="aboutajudetails" className="lead ">
-<li> Dr. Ashok Kumar, Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand </li>
-<li>Dr. Manoj Pathak , Asst. Prof., Department of English, ARKA JAIN University, Jharkhand </li>
-<li>Dr. Padmja Tripathi Asst. Prof. Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Dr. Nishikant, Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Dr. Rupa Sakar,Asst. Prof., Department of English, ARKA JAIN University, Jharkhand </li>
-<li>Dr. Anup Kumar, Asst. Prof. Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Kundan Kr Pramanik , Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand </li>
-<li>Ms. Soma Ghosh, Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Maniranjan Kumar, Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Ms. Rakhi Charaborty,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Amit Prakash Sen,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Raj Kumar Singh,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Ms. Kamilka das ,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr . Naresh Kr. Sharma,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Gaurishanker Paul,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Asish Ranjan,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Akash Kumar Bhagat Asst. Prof., Department of CSE & IT, ARKA JAIN University, Jharkhand</li>
-
+          {editorial_committee.map((field, index) => (
+            <li key={index}>{field}</li> // Use field itself or index as a key
+          ))}
           </ul>
         </div>
       </div>
@@ -127,19 +73,10 @@ export default function Committee() {
            
           </h2>
           <ul id="aboutajudetails" className="lead ">
-<li>Dr. Chandra Prabha Sahu, Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Dr. Keerti Rai, Asst. Department of Engineering, ARKA JAIN University</li>
-<li>Ms. Prerna Rai, Asst. Prof. Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Nivedan Mahato, Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Kumar Shubham, Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Syed Rashid Anwar, Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Ms. D. Vally. Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Kumaresh Pal , Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Ms. Satbhisa Sinha , Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Kuldeep Kumar Sahu , Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Md. Sazzad , Asst. Prof., Department of Engineering, ARKA JAIN University</li>
-<li>Mr. Mukesh Kumar sharma ,Asst. Prof., Department of Engineering, ARKA JAIN University, Jharkhand</li>
 
+          {organizing_committee.map((field, index) => (
+            <li key={index}>{field}</li> // Use field itself or index as a key
+          ))}
           </ul>
         </div>
       </div>
@@ -154,31 +91,23 @@ export default function Committee() {
           TECHNICAL SUPPORT COMMITTEE<span className="text-muted"></span>
           </h2>
           <ul id="aboutajudetails" className="lead ">
-<li>Mr. Asish Jha,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Ranjeet Kumar, Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Suraj Pandit,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Chandan Kumar,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mrs. Soni Kumari,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Ms. Megha Srivastava,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Ravideep ,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Neeraj Kumar,Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Ranjeet Kumar, Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Mr. Abhijit Mandal, Department of Engineering, ARKA JAIN University, Jharkhand</li>
-<li>Ms. Mallika Kumari,Department of Engineering, ARKA JAIN University, Jharkhand</li>
+          {technical_support_committee.map((field, index) => (
+            <li key={index}>{field}</li> // Use field itself or index as a key
+          ))}
 
           </ul>
         </div>
       </div>
-<div className="container">
+<div className="container w-100">
       <div class="container-fluid row my-5">
       <div class="col d-flex align-items-start my-3">
         <div class="icon-square bg-danger text-dark flex-shrink-0 me-3">
           <svg class="bi" width="1em" height="1em"></svg>
         </div>
         <div>
-          <h4 >CONVENOR</h4>
-          <p>Mr. Ashwini Kumar, Asst. Prof, ARKA JAIN University.<br/>
-          Email - ashwini.kumar@ arkajainuniversity.ac.in <br/>Mob. – +91-821035379</p>
+          <h4 >CONFERENCE CHAIR</h4>
+          <p>Dr. Ashwini Kumar, Asst. Dean, School of Engineering & IT.<br/>
+          Email - ashwini.kumar@ arkajainuniversity. ac.in <br/>Mob. – +91-8210353795</p>
           <a href="https://mail.google.com/" class="btn btn-danger " data-aos="fade-down">
           Email-Now
           </a>
@@ -193,9 +122,9 @@ export default function Committee() {
           <svg class="bi" width="1em" height="1em"></svg>
         </div>
         <div>
-          <h4 >CO- CONVENOR</h4>
-          <p>Dr. Binod Kumar Choudhary, Asst. Prof, ARKA JAIN University.<br/>
-          Email- dr.binod@ arkajainuniversity.ac .in<br/>Mob. – +91-9939974038</p>
+          <h4 >CONVENOR</h4>
+          <p>Dr. Binod Kumar Choudhary, Associate Professor, Department of Electrical & Electronics Engineering.<br/>
+          Email- dr. binod@arkajainuniversity. ac.in<br/>Mob. – +91-9939974038</p>
           <a href="https://mail.google.com/" class="btn btn-danger" data-aos="fade-down">
           Email-Now
           </a>
@@ -207,14 +136,28 @@ export default function Committee() {
   <svg class="bi" width="1em" height="1em"></svg>
 </div>
 <div>
-<h4 >Organising Secretary</h4>
-  <p>Dr. Anupam Kumari, Asst. Prof, ARKA JAIN University.<br/>
-  Email- dr.anupam@ arkajainuniversity.ac.in<br/>Mob. – +91-9939974038</p>
+<h4>CO - CONVENOR</h4>
+  <p>Dr ChandraPrabha Sahu, Assistant Professor, Department of Electrical and Electronics Engineering.<br/>
+  Email- dr.chandra@ arkajianuniversity. ac.in<br/>Mob. – +91-9128329236</p>
   <a href="https://mail.google.com/" class="btn btn-danger " data-aos="fade-down">
   Email-Now
   </a>
 </div>
+
 </div>
+<div class="col d-flex align-items-start my-3">
+        <div class="icon-square bg-danger text-dark flex-shrink-0 me-3">
+          <svg class="bi" width="1em" height="1em"></svg>
+        </div>
+        <div>
+          <h4 >CONVENOR</h4>
+          <p>Dr Jonaki, Mukherjee Assistant Professor Department of Electrical and Electronics Engineering.<br/>
+          Email- dr.jonaki@ arkajianuniversity. ac.in<br/>Mob. – +91–8902464892</p>
+          <a href="https://mail.google.com/" class="btn btn-danger" data-aos="fade-down">
+          Email-Now
+          </a>
+        </div>
+      </div>
     </div>
     </div>
    
